509b501e338c6f7c76c505b3de761e010434b540
jesserahman <jesserahman@yahoo.com>
2022-02-17 00:26:47 -0500

    added endpoint to validate username and password

--- domain/AuthRepositoryDB.go
+++ domain/AuthRepositoryDB.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"goLangAuth/dto"
@@ -15,14 +16,19 @@ type AuthRepositoryDb struct {
 func (a AuthRepositoryDb) CheckCredentials(request dto.NewAuthRequest) *errs.AppError {
 	var authQuery = fmt.Sprintf("select * from users u where username = '%s' AND password = '%s'", request.Username, request.Password)
 
-	var result string
-	err := a.dbClient.Select(&result, authQuery)
-	if err != nil {
+	var resultUser User
+	err := a.dbClient.Get(&resultUser, authQuery)
+	if err != nil && err != sql.ErrNoRows {
 		logger.Error("Error querying Accounts table " + err.Error())
 		return errs.NewUnexpectedError("unexpected db error")
 	}
 
-	fmt.Println("result is ", result)
+	if err == sql.ErrNoRows {
+		logger.Error("Error querying Accounts table " + err.Error())
+		return errs.NewNotFoundError("username and password not found")
+	}
+
+	fmt.Println("result is ", resultUser)
 
 	return nil
 }
--- domain/auth.go
+++ domain/auth.go
@@ -6,8 +6,11 @@ import (
 )
 
 type User struct {
-	Username string `db:"username"`
-	Password string `db:"password"`
+	Username   string `db:"username"`
+	Password   string `db:"password"`
+	Role       string `db:"role"`
+	CustomerId string `db:"customer_id"`
+	CreatedOn  string `db:"created_on"`
 }
 type AuthRepository interface {
 	CheckCredentials(dto.NewAuthRequest) *errs.AppError
